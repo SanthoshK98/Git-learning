4a14c563266739ecc2d1f4630529f64b44a26df4
SanthoshK98 <santhoshkumarkoneti98@gmail.com>
2022-08-25 20:09:05 +0530

    second commit on master branch

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log('feature/first branch')
+console.log('feature/first branch') 
+//it's changed like this from console.log('Master branch') due to git merge
+console.log('Now doing second commit on Master branch')
