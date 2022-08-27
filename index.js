4713dd68860ec25775ea1ce0a161c3c484748530
SanthoshK98 <santhoshkumarkoneti98@gmail.com>
2022-08-27 16:54:07 +0530

    moment package added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-
+//git pull starts
 console.log('feature/first branch')
 console.log('second commit on feature/first branch')
 
 console.log('feature/first branch') 
 //it's changed like this from console.log('Master branch') due to git merge
 console.log('Now doing second commit on Master branch')
+//git pull ends
 
-
+//git merge starts
 console.log('Git pull checking  // writing on master ')
 
 console.log("Git check line on feature/first")
+//git merge ends
 
+//installed npm moment on master
